fe32372f1102843bd3183dbd0661cf1788412e5c
Gauthier Segay <gauthier@t420s>
2014-04-08 19:09:42 +0200

    a new more efficient / accurate classifier, which works on a smoothed image of all digits rather than computing distance against each sample.

--- CodeDojos/DigitRecognizer/Script.fsx
+++ CodeDojos/DigitRecognizer/Script.fsx
@@ -46,7 +46,12 @@ let computeDataDistance (x:byte []) (y:byte []) =
 type NumberDistance = {Number: int; Distance: float}
 type ClassifierResult = {Prediction: int; Distances: NumberDistance []}
 
-let classifier (dataset:ClassificationData) (data: byte []) =
+let classifier1 (dataset:ClassificationData) (data: byte []) =
+    (*
+    this classifier is comparing distance against every sample and average the distances
+    then take the shortest averaged distance as the prediction
+    this is very slow, and not very accurate
+    *)
     let averages =
         dataset
         |> Seq.map (fun (kvp) -> (kvp.Key, kvp.Value)) // wish f# had a way to deconstruct KeyValuePair<K,V>
@@ -61,6 +66,7 @@ let classifier (dataset:ClassificationData) (data: byte []) =
         |> Seq.map (fun (k,d) -> {Number = k; Distance = d})
     {Prediction = averages |> Seq.head |> (fun nd -> nd.Number); Distances = Seq.toArray averages}
 
+
 let allRecords =
     getCsvLines trainingSampleCsvPath
     |> Seq.map convertStringToRecord
@@ -68,33 +74,65 @@ let allRecords =
 
 let trainingDataset = createProcessingDataSet allRecords
 
-let oneRecord = { Data= trainingDataset.[1].[100]; Number= -11}
+let averageData (data: byte[] seq) =
+    let count = Seq.length data
+    let firstArrayLength = (Seq.head data).Length
+    let zero = Array.init<uint64> firstArrayLength (fun _ -> 0 |> uint64)
+    data
+    |> Seq.map (fun d -> d |> Array.map uint64)
+    |> Seq.fold (fun prev cur -> Array.map2 (+) prev cur) zero
+    |> Seq.map (float)
+    |> Seq.map (fun v -> v / (count |> float))
+    |> Seq.map (byte)
+    |> Seq.toArray
 
-//> classifier trainingDataset oneRecord
+let mixedTrainingDatasetToSingleImage = 
+    trainingDataset 
+    |> Seq.map (fun kvp -> (kvp.Key, kvp.Value))
+    |> Seq.map (fun (k,v) -> (k, averageData v))
+    |> dict
+
+let classifier2 (data: byte[]) =
+    (*
+    this classifier works on a simplified training dataset where all images are averaged together 
+    (for each of 10 digits, only a single smoothed image to match on)
+    far quicker than classifier1 (compute only 10 distances rather than computing it for every sample) 
+    and also far more accurate in results
+    *)
+    let predictions = 
+        mixedTrainingDatasetToSingleImage
+        |> Seq.map (fun kvp -> (kvp.Key, computeDataDistance kvp.Value data))
+        |> Seq.sortBy (fun (k,d) -> d)
+    {Prediction = (predictions |> Seq.head |> fst) ; Distances = predictions |> Seq.map (fun (k,d) -> {Number = k; Distance = d}) |> Seq.toArray }
+
+let oneRecord = { Data= trainingDataset.[1].[100]; Number= -11}
 
 let records = getCsvLines trainingSampleCsvPath
                 |> Seq.map convertStringToRecord
                 |> Seq.map (fun r -> r.Number)
                 |> Seq.distinct
                 |> Seq.iter (fun v -> Console.WriteLine(v))
-(* // compute distance samples
-let d1 = [|1 |> byte; 2 |> byte|]
-let d2 = [|0 |> byte; 5 |> byte|]
-computeDataDistance d1  d2
-*) //
 
 let rateClassifier (data: DataRecord seq) (predictor: (byte[] -> int)) (dataset: ClassificationData) =
     let count = Seq.length data |> float
+    let counter = ref 0
+    let matched = ref 0
+    let mismatched = ref 0
     let predictions = 
         data
         |> Seq.map (fun d -> 
             let prediction = predictor d.Data
+            counter := (!counter + 1) 
+            if prediction = d.Number then matched := (!matched + 1)
+            else mismatched := (!mismatched + 1)
+            Console.WriteLine("processed {0} (matched: {1}, mismatched: {2})", !counter, !matched, !mismatched)
             (d.Number = prediction, prediction)
         )
     let correctAnswers = predictions |> Seq.filter (fun (isMatch, _) -> isMatch) |> Seq.length |> float
     correctAnswers / count
 
-rateClassifier (allRecords |> Seq.take 100) (fun r -> (classifier trainingDataset r).Prediction) trainingDataset
+rateClassifier (allRecords |> Array.toSeq) (fun r -> (classifier1 trainingDataset r).Prediction) trainingDataset
+rateClassifier (allRecords |> Array.toSeq) (fun r -> (classifier2 r).Prediction) trainingDataset
 
 open DigitRecognizer
 DigitRecognizer.batch [|1..100|] 10
@@ -108,7 +146,7 @@ let showImage (image: Image) =
     form.Controls.Add(imageDisplay)
     form.Show()
 
-let displayData (data: byte[]) (n: int) =
+let displayData (n: int)  (data: byte[]) =
     let rows = data.Length / n + (if data.Length % n > 0 then 1 else 0)
     let image = new Bitmap(n, rows)
     let twoDimensionData = DigitRecognizer.batch data n |> Seq.zip [0..(rows)]
@@ -117,10 +155,11 @@ let displayData (data: byte[]) (n: int) =
         pixels |> Seq.iter (fun (x, v) -> image.SetPixel(x, y, Color.FromArgb(v |> int, v |> int, v |> int)))
     showImage image
 
-displayData [|(0|> byte)..(255|> byte)|] 10
-displayData oneRecord.Data 28
+displayData 10 [|(0|> byte)..(255|> byte)|]
+displayData 28 oneRecord.Data
+averageData trainingDataset.[9] |> displayData 28
 
-trainingDataset.[9] |> Seq.take 10 |> Seq.map (fun d -> displayData d 28)
+trainingDataset.[9] |> Seq.take 10 |> Seq.map (fun d -> displayData 28 d)
 
 // visualise distance in a naive way
 let showAbsoluteDistance (x: byte[]) (y: byte[]) =
@@ -130,5 +169,5 @@ let showAbsoluteDistance (x: byte[]) (y: byte[]) =
         |> Seq.map Math.Abs
         |> Seq.map byte
         |> Seq.toArray
-    displayData distance 28
+    displayData 28 distance
 //> showAbsoluteDistance trainingDataset.[9].[0] trainingDataset.[9].[50]
