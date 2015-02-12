0af1f97a753a0be56804039ba8f2077517046aaf
Gauthier Segay <gauthier@t420s>
2015-02-12 14:23:24 +0100

    TagUnionCaseReflection additions
    add TagUnionCaseReflection.GetName
    add test for TagUnionCaseReflection.GetName

--- smoothdev.Utils/src/smoothdev.FSharp.Reflection/TagUnionCaseReflection.fs
+++ smoothdev.Utils/src/smoothdev.FSharp.Reflection/TagUnionCaseReflection.fs
@@ -1,15 +1,22 @@
 namespace smoothdev.FSharp.Reflection
-
+open System.Collections.Generic
 open Microsoft.FSharp.Reflection
 
-type TagUnionCaseReflection<'T> =
+type TagUnionCaseReflection<'T> when 'T : equality =
   { CaseInfo: UnionCaseInfo
   ; Value: 'T
   }
   with
-    // will crash if 'T contains members which aren't only tags
-    static member construct (caseInfo: UnionCaseInfo)         = FSharpValue.MakeUnion(caseInfo, [||]) :?> 'T
-  
-    static member AllCases = 
+    static member unionCasesLookup =
+      let dic = new Dictionary<_,_>()
       FSharpType.GetUnionCases(typeof<'T>)
-      |> Seq.map (fun info -> {CaseInfo= info; Value = TagUnionCaseReflection<'T>.construct info})
+      |> Seq.map (fun t -> (TagUnionCaseReflection<'T>.construct t, TagUnionCaseReflection<'T>.makeCase t))
+      |> Seq.iter dic.Add
+      dic
+
+    // will crash if 'T contains members which aren't only tags
+    static member construct (caseInfo: UnionCaseInfo)    = FSharpValue.MakeUnion(caseInfo, [||]) :?> 'T
+    static member makeCase (caseInfo: UnionCaseInfo)     = {CaseInfo = caseInfo; Value = TagUnionCaseReflection<'T>.construct caseInfo}
+    static member GetName (tag: 'T)                      = TagUnionCaseReflection.unionCasesLookup.[tag].CaseInfo.Name
+    static member AllCases                               = TagUnionCaseReflection<'T>.unionCasesLookup.Values :> seq<_>
+      
--- smoothdev.Utils/tests/smoothdev.FSharp.Reflection.Tests.fs
+++ smoothdev.Utils/tests/smoothdev.FSharp.Reflection.Tests.fs
@@ -1,16 +1,23 @@
 module ``smoothdev FSharp Reflection Tests``
 
-open smoothdev.FSharp.Reflection
-open NUnit.Framework
 open FsUnit
+open NUnit.Framework
+open smoothdev.FSharp.Reflection
 
 type Tags = A | B | C | D
 
+type reflect<'T> when 'T : equality = TagUnionCaseReflection<'T>
+
 [<Test>]
 let ``can reflect Tag type discriminated union`` () =  
-  let cases = smoothdev.FSharp.Reflection.TagUnionCaseReflection<Tags>.AllCases
-  
-  let expected = [A;B;C;D]
+  let cases = reflect<Tags>.AllCases
+  let expected = [ A; B; C; D ]
   let actual = cases |> Seq.map (fun c -> c.Value)
-  expected |> should equal actual 
-  printfn "%O %O" actual expected
+  expected |> should equal actual
+  assert ((Seq.length actual) = 4)
+
+[<Test>]
+let ``can get name``() = 
+  let actual = reflect<Tags>.AllCases |> Seq.map (fun c -> reflect<Tags>.GetName(c.Value))
+  let expected = [ "A"; "B"; "C"; "D" ]
+  expected |> should equal actual
