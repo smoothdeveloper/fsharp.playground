5c7200aac83f3bdf6c77e0e73ef42418de9b89db
Gauthier Segay <gauthier.segay@gmail.com>
2016-01-09 05:35:10 -0800

    add Option reflection helper functions for boxed values

---
+++ smoothdev.Utils/src/smoothdev.FSharp.Reflection/Option.fs
@@ -0,0 +1,22 @@
+﻿module Option
+
+let private getOptionType t =
+    let optionGenericType = typedefof<FSharp.Core.option<_>>
+    optionGenericType.MakeGenericType([|t|])
+
+let toOptionNone t =
+  // remark could be replaced with
+  // toOptionNone t :obj = null 
+  let optionType = getOptionType t
+  let cases = FSharp.Reflection.FSharpType.GetUnionCases(optionType)
+  let noneFsharpType = cases |> Seq.find(fun i -> i.Name = "None")
+  FSharp.Reflection.FSharpValue.MakeUnion(noneFsharpType, [||])
+  
+let toOptionSomeFromType t v =
+  let optionType = getOptionType (t)
+  let cases = FSharp.Reflection.FSharpType.GetUnionCases(optionType)
+  let someFsharpType = cases |> Seq.find(fun i -> i.Name = "Some")
+  FSharp.Reflection.FSharpValue.MakeUnion(someFsharpType, [|v|])
+
+let toOptionSome v =
+  toOptionSomeFromType (v.GetType()) v
--- smoothdev.Utils/tests/smoothdev.FSharp.Reflection.Tests.fs
+++ smoothdev.Utils/tests/smoothdev.FSharp.Reflection.Tests.fs
@@ -1,30 +1,47 @@
-module ``smoothdev FSharp Reflection Tests``
+namespace smoothdev.FSharp.Reflections.Tests
 
 open FsUnit
 open NUnit.Framework
 open smoothdev.FSharp.Reflection
 
-type Tags = A | B | C | D
+module ``TagUnionCaseReflection tests`` =
 
-type reflect<'T> when 'T : equality = TagUnionCaseReflection<'T>
+  type Tags = A | B | C | D
 
-[<Test>]
-let ``can reflect Tag type discriminated union`` () =  
-  let cases = reflect<Tags>.AllCases
-  let expected = [ A; B; C; D ]
-  let actual = cases |> Seq.map (fun c -> c.Value)
-  expected |> should equal actual
-  assert ((Seq.length actual) = 4)
+  type reflect<'T> when 'T : equality = TagUnionCaseReflection<'T>
 
-[<Test>]
-let ``can get name``() = 
-  let actual = reflect<Tags>.AllCases |> Seq.map (fun c -> reflect<Tags>.GetName(c.Value))
-  let expected = [ "A"; "B"; "C"; "D" ]
-  expected |> should equal actual
+  [<Test>]
+  let ``can reflect Tag type discriminated union`` () =  
+    let cases = reflect<Tags>.AllCases
+    let expected = [ A; B; C; D ]
+    let actual = cases |> Seq.map (fun c -> c.Value)
+    expected |> should equal actual
+    assert ((Seq.length actual) = 4)
 
-[<Test>]
-let ``try get name`` () = 
-  let caseA = reflect<Tags>.TryGet "a" true
-  assert (caseA = Some A)
-  let caseA = reflect<Tags>.TryGet "a" false
-  assert (caseA = None)
+  [<Test>]
+  let ``can get name``() = 
+    let actual = reflect<Tags>.AllCases |> Seq.map (fun c -> reflect<Tags>.GetName(c.Value))
+    let expected = [ "A"; "B"; "C"; "D" ]
+    expected |> should equal actual
+
+  [<Test>]
+  let ``try get name`` () = 
+    let caseA = reflect<Tags>.TryGet "a" true
+    assert (caseA = Some A)
+    let caseA = reflect<Tags>.TryGet "a" false
+    assert (caseA = None)
+
+module ``Option tests`` =
+
+  [<Test>]
+  let ``toOptionSome of boxed value returns expected type`` () =
+    let value = box 3
+    let expected = Some 3
+    let actual = Option.toOptionSome value
+    expected |> should equal actual
+
+  [<Test>]
+  let ``toOptionNone of runtime type gives expected None`` () =
+    let expected : int option = None
+    let actual = Option.toOptionNone typeof<int>
+    expected |> should equal actual
